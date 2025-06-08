56e7ec46043d341bb098f75667421a50ae7799d8
DJjanko <jankladusek7@gmail.com>
2025-06-08 13:11:20 +0200

    login test

--- backend/test/login.test.js
+++ backend/test/login.test.js
@@ -1,15 +1,17 @@
 const request = require('supertest');
 const app = require('../app');
+const mongoose = require('mongoose');  // Import mongoose
 
 describe('User Login', () => {
-    it('should fail login with wrong credentials', (done) => {
-        request(app)
+    after(() => {
+        // Close mongoose connection after tests finish
+        mongoose.connection.close();
+    });
+
+    it('should fail login with wrong credentials', async () => {
+        await request(app)
             .post('/users/login')
             .send({ username: 'nonexistent', password: 'wrongpass' })
-            .expect(401)
-            .end((err, res) => {
-                if (err) return done(err);
-                done();
-            });
+            .expect(401);
     });
 });
